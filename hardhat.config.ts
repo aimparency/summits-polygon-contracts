08dcd2faf93fd5a794852a30fd7a35e324fa259f
Felix Niemeyer <niemeyer.felix@gmail.com>
2022-06-21 13:01:37 +0200

    localnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -30,11 +30,6 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-    local_eth: {
-      url: "http://127.0.0.1:8545/", 
-      accounts: ['ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'], 
-      chainId: 1337
-    }, 
     hardhat: {
       chainId: 1337
     }
